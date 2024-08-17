1acc73c98ff95a3999736214b46fec91e3f42db8
HNG <fr.quochien@gmail.com>
2024-08-17 20:57:36 +0700

    access doc with swagger from api-gateway

--- auth-service/src/main/java/com/meta/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/meta/authservice/AuthServiceApplication.java
@@ -1,11 +1,16 @@
 package com.meta.authservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+/*@OpenAPIDefinition(info =
+@Info(title = "Auth API", version = "${springdoc.version}", description = "Documentation Auth API v1.0")
+)*/
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
